2d3355c0d4e1debbd6cc19af662f4a2a630e611a
YismaelDiazH <20213tn007@utez.edu.mx>
2025-05-27 15:51:36 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/mx/edu/utez/cvaydh9b/Cvaydh9bApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvaydh9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvaydh9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvaydh9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvaydh9b/Cvaydh9bApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvaydh9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvaydh9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
